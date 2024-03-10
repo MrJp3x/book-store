0fda48fe3ef89bed1d0dc99cd709e4a976806386
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-03-10 22:54:47 +0330

    feat(book): Added urls for book, author and category.

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import HandleAuthor, HandleBookCategory, HandleBooks
+
+
+router = DefaultRouter()
+router.register(r'book', HandleBooks)
+router.register(r'category', HandleBookCategory)
+router.register(r'author', HandleAuthor)
+
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
