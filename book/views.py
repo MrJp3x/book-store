7c80af3e28e350e9bd79ca42eef5d9ef66f8ae25
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-03-10 22:54:19 +0330

    feat(book): Created views for book, author and category.

--- book/views.py
+++ book/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Book, BookCategory, Author
+from .serializers import BooksSerializer, BooksCategorySerializer, AuthorSerializer
+
+
+class HandleBooks(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
+
+
+class HandleBookCategory(ModelViewSet):
+    queryset = BookCategory.objects.all()
+    serializer_class = BooksCategorySerializer
+
+
+class HandleAuthor(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
