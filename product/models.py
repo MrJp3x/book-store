1e35a0baf1bde540089f60878b7bac710df7221c
Pedram <amirabbasasgary9731@gmail.com>
2024-08-03 14:36:03 +0330

    add: Product model created.

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=False, blank=False, null=False)
+    category = models.CharField(max_length=120, blank=False, null=False)
+    price = models.FloatField(blank=False, null=False)
+    discount = models.FloatField(blank=True, null=True, default=0)
+    description = models.TextField(blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True, default=0)
+
+    def __str__(self):
+        return f'{self.category} / {self.name} / {self.quantity}'
