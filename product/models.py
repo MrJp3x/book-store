86f74addfe3f5ff29b721f485fa72eb7d6a92497
Pedram <amirabbasasgary9731@gmail.com>
2024-08-04 15:02:38 +0330

    update: update category app.

--- product/models.py
+++ product/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from category.models import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=False, blank=False, null=False)
-    category = models.CharField(max_length=120, blank=False, null=False)
+    # category = models.CharField(max_length=120, blank=False, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.FloatField(blank=False, null=False)
     discount = models.FloatField(blank=True, null=True, default=0)
     description = models.TextField(blank=True, null=True)
