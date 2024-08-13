e3bd966d01db188c3cb9d780455148b8a6a09313
Pedram <amirabbasasgary9731@gmail.com>
2024-08-13 22:21:16 +0330

    Manage categories as parent-child

--- category/admin.py
+++ category/admin.py
@@ -1,12 +1,44 @@
 from django.contrib import admin
+from .models import Category
 
-from category.models import Category
+
+class BaseCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id',
+                    'name',
+                    'parent',
+                    ]
+    list_filter = ('parent',)
+    search_fields = ('name',)
+
+    class Media:
+        """
+        اضافه کردن CSS سفارشی برای بهتر کردن نمایش سلسله‌مراتب.
+        """
+        css = {
+            'all': ('css/admin_custom.css',)
+        }
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'parent']
-    list_filter = ['parent']
-    search_fields = ['name']
+class CategoryAdmin(BaseCategoryAdmin):
+    list_display = BaseCategoryAdmin.list_display + ["category_hierarchy"]
+
+    def category_hierarchy(self, obj):
+        """
+        نمایش سلسله‌مراتبی کتگوری‌ها.
+        """
+        ancestors = obj.get_ancestors()
+        if ancestors:
+            return ' > '.join([ancestor.name for ancestor in ancestors] + [obj.name])
+        return obj.name
+
+    category_hierarchy.short_description = "Category Hierarchy"
+
+    def get_queryset(self, request):
+        """
+        کوئری‌ست‌های پنل ادمین را سفارشی ‌سازی می‌کنیم تا کتگوری‌ها بر اساس سلسله‌مراتب مرتب شوند.
+        """
+        queryset = super().get_queryset(request)
+        return queryset.order_by('parent__id', 'id')
 
 
 admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -8,3 +8,30 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+
+    def get_ancestors(self):
+        """
+        Returns a list of ancestors for this category, starting from the root.
+        """
+        ancestors = []
+        category = self
+        while category.parent:
+            ancestors.insert(0, category.parent)
+            category = category.parent
+        return ancestors
+
+    def get_descendants(self):
+        """
+        Returns a list of descendants for this category.
+        """
+        descendants = []
+        children = self.children.all()
+        for child in children:
+            descendants.append(child)
+            descendants.extend(child.get_descendants())
+        return descendants
+
+    class Meta:
+        ordering = ['name']
