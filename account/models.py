2149699b9ec98442000ab60fb336f05ecc0f472a
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-09-04 00:46:41 +0330

    ref(account): Use text choice for user const in user model.

--- account/models.py
+++ account/models.py
@@ -2,13 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    USER_ROLE = (
-        ('normal', 'User'),
-        ('publisher', 'Book Publisher'),
-    )
+class UserConst(models.TextChoices):
+    NORMAL = 'normal', 'User'
+    PUBLISHER = 'publisher', 'Book Publisher'
 
-    role = models.CharField(max_length=20, choices=USER_ROLE)
+
+class User(AbstractUser):
+
+    role = models.CharField(max_length=20, choices=UserConst.choices)
     phone = models.CharField(max_length=11, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
 
