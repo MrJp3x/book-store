541bc3285e4799519d9f4510ea57a31867045151
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-09-04 00:25:06 +0330

    feat(account): Added email field in user model.

--- account/models.py
+++ account/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     role = models.CharField(max_length=20, choices=USER_ROLE)
     profile_picture = models.ImageField(upload_to='static/profile_pictures/', blank=True, null=True)
     phone = models.CharField(max_length=11, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.username
