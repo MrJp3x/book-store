3969ec9ed37b6bc48c4e22369f6568111dfdeb65
Pedram <amirabbasasgary9731@gmail.com>
2024-09-16 21:30:37 +0330

    feat(settings): Added RestFramework settings.

--- book_store/settings.py
+++ book_store/settings.py
@@ -134,3 +134,10 @@ AUTH_USER_MODEL = "account.User"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
