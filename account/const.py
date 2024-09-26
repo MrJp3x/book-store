cb9a70591798d7309ea3e324fb8fff481afc7c1d
Pedram <amirabbasasgary9731@gmail.com>
2024-09-26 23:43:28 +0330

    feat(account): Added user and profile models.added const file.

---
+++ account/const.py
@@ -0,0 +1,3 @@
+admin_type_choice = ('content_admin', 'user_admin', 'tech_admin', 'financial_admin',)
+user_type_choice = ('user', 'publisher', 'admin')
+sex_choice = ('Femail', 'Male', 'Rather to not say.')
