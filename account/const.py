a552234fb0e9928d7d0f4d78e726b58a0b43855c
Pedram <amirabbasasgary9731@gmail.com>
2024-09-27 21:06:51 +0330

    refactor: improve the structure of choices in const.py

--- account/const.py
+++ account/const.py
@@ -1,3 +1,16 @@
-admin_type_choice = ('content_admin', 'user_admin', 'tech_admin', 'financial_admin',)
-user_type_choice = ('user', 'publisher', 'admin')
-sex_choice = ('Femail', 'Male', 'Rather to not say.')
+ADMIN_TYPE_CHOICES = [
+    ('content_admin', 'Content Admin'),
+    ('user_admin', 'User Admin'),
+    ('tech_admin', 'Tech Admin'),
+    ('financial_admin', 'Financial Admin'),
+]
+USER_TYPE_CHOICES = [
+    ('user', 'User'),
+    ('publisher', 'Publisher'),
+    ('admin', 'Admin'),
+]
+SEX_CHOICES = [
+    ('F', 'Female'),
+    ('M', 'Male'),
+    ('N', 'Rather not say'),
+]
