2ab499ab4109a0b7f39501bafc2a5c6ce079bfb4
Pedram <amirabbasasgary9731@gmail.com>
2024-09-27 22:15:45 +0330

    ref(account): Refactor cont variable to class form.

--- account/const.py
+++ account/const.py
@@ -1,16 +1,17 @@
-ADMIN_TYPE_CHOICES = [
-    ('content_admin', 'Content Admin'),
-    ('user_admin', 'User Admin'),
-    ('tech_admin', 'Tech Admin'),
-    ('financial_admin', 'Financial Admin'),
-]
-USER_TYPE_CHOICES = [
-    ('user', 'User'),
-    ('publisher', 'Publisher'),
-    ('admin', 'Admin'),
-]
-SEX_CHOICES = [
-    ('F', 'Female'),
-    ('M', 'Male'),
-    ('N', 'Rather not say'),
-]
+class AccountConst:
+    ADMIN_TYPE_CHOICES = [
+        ('content_admin', 'Content Admin'),
+        ('user_admin', 'User Admin'),
+        ('tech_admin', 'Tech Admin'),
+        ('financial_admin', 'Financial Admin'),
+    ]
+    USER_TYPE_CHOICES = [
+        ('user', 'User'),
+        ('publisher', 'Publisher'),
+        ('admin', 'Admin'),
+    ]
+    SEX_CHOICES = [
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('N', 'Rather not say'),
+    ]
