f75a52543c26c82642be3358eeaf7b2822f993bc
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-10-04 19:55:30 +0330

    ref(product): Changed product table to abstract and added admin pannel.

--- product/admin.py
+++ product/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Product
+from .models import PhysicalBook, EBook, AudioBook, ProductType
 
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'category', 'quantity']
-    search_fields = ['name']
-    list_filter = ['category', 'price']
+class BaseCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 
-admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductType, BaseCategoryAdmin)
+
+
+@admin.register(PhysicalBook)
+class PhysicalBookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'weight', 'dimensions', 'cover_type', 'number_of_pages')
+    search_fields = ('name',)
+
+@admin.register(EBook)
+class EBookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file_format', 'file_size', 'number_of_pages')
+    search_fields = ('name',)
+
+@admin.register(AudioBook)
+class AudioBookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'audio_length', 'narrator', 'episode_count')
+    search_fields = ('name', 'narrator')
--- product/models.py
+++ product/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.urls import reverse
 
 from utils.models import TimeStamp
+from category.models import Category
+
+
+# region ProductType
+class ProductType(TimeStamp):
+    """
+    Define product type. For example is the product e-book, v-book or ...
+
+    Attributes:
+        name (models.CharField): The name of the product.
+    """
+
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
+
+# endregion
 
 
 # region Product
@@ -11,25 +28,28 @@ class Product(TimeStamp):
 
     Attributes:
         name (models.CharField): The name of the product.
+        product_type (models.ForeignKey): Product type of the product
         price (models.FloatField): The price of the product.
         discount (models.FloatField): The discount on the product, if any.
         stock (models.PositiveIntegerField): The number of items available in stock.
         description (models.TextField): A detailed description of the product.
         rating (models.FloatField): The rating of the product.
+        is_available (models.BooleanField): Is product available or not.
 
     """
 
     name = models.CharField(max_length=100, unique=False, blank=False, null=False)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
     price = models.FloatField(blank=False, null=False)
     discount = models.FloatField(blank=True, null=True, default=0)
     stock = models.PositiveIntegerField(default=0, help_text="Number of items in stock")
     description = models.TextField(blank=True, null=True)
     rating = models.FloatField(blank=True, null=True)
+    is_available = models.BooleanField(default=True)
 
 
     class Meta:
-        # Enable multi-table inheritance
-        abstract = False
+        abstract = True
 
 
     def __str__(self):
@@ -44,36 +64,6 @@ class Product(TimeStamp):
 # endregion
 
 
-# region BookCategory
-class BookCategory(models.Model):
-    """
-    Represents a category for products, such as Fiction, Non-Fiction, Accessories, etc.
-
-     Attributes:
-        name (models.CharField): The name of the category.
-        slug (models.SlugField): A URL-friendly identifier for the category.
-
-    """
-
-    name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True, help_text="URL-friendly identifier for the category")
-
-    class Meta:
-        verbose_name_plural = 'Categories'
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        """
-        Returns the URL to access a list of products in this category.
-        """
-
-        return reverse('product:product_list_by_category', args=[self.slug])
-
-# endregion
-
-
 # region Book
 class Book(Product):
     """
@@ -84,31 +74,24 @@ class Book(Product):
         author (models.CharField): The author of the book.
         translator (models.CharField): The translator name of the book.
         publication_name (models.CharField): The publication name of the book.
-        category (models.ForeignKey): The category to which the book belongs.
+        categories (models.ForeignKey): The category to which the book belongs.
         slug (models.SlugField): A URL-friendly identifier for the book.
         cover_image (models.ImageField): The cover image of the book.
         ISBN (models.CharField): The ISBN number.
         subject (models.CharField): The subject of the book.
 
     """
-
     author = models.CharField(max_length=100, help_text="Author of the book")
     translator = models.CharField(max_length=100, blank=True, null=True)
     publication_name = models.CharField(max_length=100, blank=True, null=True)
-    category = models.ForeignKey(BookCategory, related_name='books', on_delete=models.CASCADE)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=200, unique=True, help_text="URL-friendly identifier for the book")
     cover_image = models.ImageField(upload_to='book_covers/', help_text="Cover image of the book")
     ISBN = models.CharField(max_length=25, blank=True, null=True)
     subject = models.CharField(max_length=250, blank=True, null=True)
 
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        """
-        Returns the URL to access the detail view of the book.
-        """
-        return reverse('product:book_detail', args=[self.slug])
+    class Meta:
+        abstract = True
 
 # endregion
 
