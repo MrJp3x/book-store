385a7f1dcd548321f7df1a9948da703a11736aad
Pedram <amirabbasasgary9731@gmail.com>
2024-10-08 19:09:32 +0330

    feat(book_store): Added templates directory to settings. added home_module settings and set home url.

--- book_store/urls.py
+++ book_store/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', include('home_module.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 ]
