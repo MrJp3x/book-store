5c4828d83cce4f511edc6858bae4e621e6479766
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-11-08 21:25:45 +0330

    feat(url): Added category and product urls in url file.

--- book_store/urls.py
+++ book_store/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('category/', include('category.urls')),
+    path('product/', include('product.urls')),
 ]
