af375498988f81ce15852292a6eb4eaf8b6d7bad
Fateme-Beshkouh <fatemeh.beshkouh@gmail.com>
2024-11-25 21:03:11 +0330

    feat(product:serializers): Added validate price and stock method.

--- product/serializers.py
+++ product/serializers.py
@@ -7,6 +7,15 @@ class BaseBookSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('id', 'created_at', 'updated_at')
 
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0")
+
+        return value
+
+    def validate_stock(self, value):
+        if value < 0 :
+            raise serializers.ValidationError("Stock cannot be negative.")
 
 class ProductTypeSerializer(BaseBookSerializer):
     class Meta:
